1e3190475fd873c1af6cf6168e08ce8655f16f36
TrencsanszkyPatrik <trencsanszkyp@kkszki.hu>
2025-09-29 09:29:40 +0200

    Register working, added all inputs to the window

--- computershop/RegisterWindow.xaml.cs
+++ computershop/RegisterWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,49 @@ namespace computershop
         public RegisterWindow()
         {
             InitializeComponent();
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = usernameBox.Text;
+            string fullname = fullnameBox.Text;
+            string email = emailBox.Text;
+            string password = passwordBox.Password;
+
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password) || string.IsNullOrWhiteSpace(fullname))
+            {
+                MessageBox.Show("Kérlek tölts ki minden mezőt!");
+                return;
+            }
+
+            Connect db = new Connect("users");
+
+            try
+            {
+                db.Connection.Open();
+
+                MySqlCommand cmd = new MySqlCommand(
+                    "INSERT INTO Users (Username,Fullname,  Email, Password) VALUES (@username, @FullName,  @email, @password)",
+                    db.Connection
+                );
+
+                cmd.Parameters.AddWithValue("@username", username);
+                cmd.Parameters.AddWithValue("@FullName", fullname);
+                cmd.Parameters.AddWithValue("@email", email);
+                cmd.Parameters.AddWithValue("@password", password); 
+
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Sikeres regisztráció!");
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Hiba az adatbázisban: " + ex.Message);
+            }
+            finally
+            {
+                db.Connection.Close();
+            }
         }
     }
 }
