05b6cf010cda443679660a092d15ad6eb00570d2
TrencsanszkyPatrik <trencsanszkyp@kkszki.hu>
2025-10-06 11:10:15 +0200

    crud buttons added to adminpage, hashed passwords

--- computershop/Services/Users.cs
+++ computershop/Services/Users.cs
@@ -1,8 +1,10 @@
 ﻿using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Common;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,29 +17,34 @@ namespace computershop.Services
 
         public object AddRecord(string username, string fullname, string email, string password)
         {
+            string salt = GenerateSalt();
+            string hashedPassword = ComputeHmacSha256(password, salt);
             conn.Connection.Open();
 
-            string sql = "INSERT INTO `users`( `UserName`, `FullName`, `Password`, `Email`) VALUES (@username, @fullname, @password, @email)";
+            string sql = "INSERT INTO `users`(`UserName`, `FullName`, `Password`, `Email`, `Salt`) VALUES (@username,@fullname,@password,@email,@salt)";
+
             MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
             cmd.Parameters.AddWithValue("@username", username);
-            cmd.Parameters.AddWithValue("@password", password);
             cmd.Parameters.AddWithValue("@fullname", fullname);
             cmd.Parameters.AddWithValue("@email", email);
+            cmd.Parameters.AddWithValue("@password", hashedPassword);
+            cmd.Parameters.AddWithValue("@salt", salt);
 
             cmd.ExecuteNonQuery();
-
-
             conn.Connection.Close();
-            
-            return new { message = "sikeres" };
+
+            return new { message = "Sikeres regisztráció!" };
         }
+
 
         public DataView GetAllData()
         {
             conn.Connection.Open();
+
             string sql = "SELECT * FROM users";
 
-            MySqlDataAdapter adapter = new(sql, conn.Connection);
+            MySqlDataAdapter adapter = new MySqlDataAdapter(sql, conn.Connection);
+
             DataTable dt = new DataTable();
 
             adapter.Fill(dt);
@@ -51,27 +58,47 @@ namespace computershop.Services
         {
             conn.Connection.Open();
 
-            string sql = "SELECT * FROM users WHERE UserName = @username AND Password = @password";
+            string sql = "SELECT * FROM users WHERE UserName = @username";
+
             MySqlCommand cmd = new MySqlCommand(sql, conn.Connection);
             cmd.Parameters.AddWithValue("@username", username);
-            cmd.Parameters.AddWithValue("@password", password);
 
-
-            bool result = false;
             MySqlDataReader reader = cmd.ExecuteReader();
+            bool result = false;
+
             if (reader.Read())
             {
+                string storedHash = reader.GetString(3);
+                string storedSalt = reader.GetString(6);
+                string compteHash = ComputeHmacSha256(password, storedSalt);
                 result = true;
+                conn.Connection.Close();
+
             }
-            else result = false;
-
-
-
             conn.Connection.Close();
-
             return result;
-               
-            ;
+
+        }
+
+        public string GenerateSalt()
+        {
+            byte[] salt = new byte[16];
+
+            using (var rnd = RandomNumberGenerator.Create())
+            {
+                rnd.GetBytes(salt);
+            }
+
+            return Convert.ToBase64String(salt);
+        }
+
+        public string ComputeHmacSha256(string password, string salt)
+        {
+            using (var hmac = new HMACSHA256(Encoding.UTF8.GetBytes(salt)))
+            {
+                byte[] hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return Convert.ToBase64String(hash);
+            }
         }
     }
 }
